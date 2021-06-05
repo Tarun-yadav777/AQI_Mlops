1f18b5e7736ae7e3a92d7cb1c040f38a81bd897f
Tarun <tarun.rao777@gmail.com>
2021-06-05 14:25:16 +0530

    Get data function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,23 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import yaml
+import pandas as pd
+import argparse
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def read_parameter(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+        return config
+
+
+def get_data(config_path):
+    config = read_parameter(config_path)
+    data_path = config["data_source"]["source"]
+    df = pd.read_csv(data_path)
+    return df
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml", )
+    parsed_args = args.parse_args()
+    data = get_data(config_path=parsed_args.config)
